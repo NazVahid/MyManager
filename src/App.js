e95b5e26f75659b21a805168ba5961c59417991b
Naz.Vahid <naz.vahid@gmail.com>
2021-08-16 12:12:51 +0430

    person manager redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Persons from "./components/Person/Persons";
+import Header from "./components/common/Header";
+import NewPerson from "./components/Person/NewPerson";
+import { useSelector, useDispatch } from "react-redux";
+import { setShowPersons } from "./actions/showPersons";
 
+const App = () => {
+    const showPersons = useSelector(state => state.showPersons);
+    const dispatch = useDispatch();
+    return (
+        <div className="rtl text-center">
+            <Header appTitle="مدیریت کننده اشخاص" />
+
+            <NewPerson />
+
+            <Button
+                onClick={() => dispatch(setShowPersons())}
+                variant={showPersons ? "info" : "danger"}
+            >
+                نمایش اشخاص
+            </Button>
+
+            {showPersons ? <Persons /> : null}
+        </div>
+    );
+};
 export default App;
